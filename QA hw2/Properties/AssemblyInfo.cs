329fa13f862442a78143aa35abbb4da952cee486
Marco Serrano <marcoTheWebDev@gmail.com>
2019-09-12 11:18:29 -0500

    Quick C# automation of Selenium QA testing webdriver.

---
+++ QA hw2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("QA hw2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("QA hw2")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0881e003-c6a7-4593-906b-cdfb1ad294ed")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ QA hw2/UnitTest1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+
+namespace QA_hw2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public static IWebDriver WebDriver;
+        public static string baseURL = "http://www.google.com.mx/";
+
+        [TestMethod]
+        public void FirefoxTest()
+        {
+            using (var WebDriver = new FirefoxDriver())
+            {
+                WebDriver.Manage().Window.Maximize();
+
+                WebDriver.Navigate().GoToUrl(baseURL);
+
+                var searchBox = WebDriver.FindElementByName("q");
+
+                searchBox.SendKeys("automation using selenium 3.0 in C#");
+
+                searchBox.Submit();
+
+                WebDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+                
+                var searchResults = WebDriver.FindElementById("resultStats");
+
+                WebDriver.Close();
+            }
+        }
+
+        [TestMethod]
+        public void ChromeTest()
+        {
+            using (var WebDriver = new ChromeDriver())
+            {
+                WebDriver.Manage().Window.Maximize();
+
+                WebDriver.Navigate().GoToUrl(baseURL);
+
+                var searchBox = WebDriver.FindElementByName("q");
+
+                searchBox.SendKeys("Automation using selenium 3.0 in C#");
+
+                searchBox.Submit();
+
+                var searchResults = WebDriver.FindElementById("resultStats");
+
+                WebDriver.Close();
+            }
+        }
+    }
+}
