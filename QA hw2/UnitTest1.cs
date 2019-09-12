5a877dc0478b39cfa277e4faa53dbdfb43e6da20
Marco Serrano <marcoTheWebDev@gmail.com>
2019-09-12 11:53:58 -0500

    Personalized program for a specific form.

--- QA hw2/UnitTest1.cs
+++ QA hw2/UnitTest1.cs
@@ -10,7 +10,7 @@ namespace QA_hw2
     public class UnitTest1
     {
         public static IWebDriver WebDriver;
-        public static string baseURL = "http://www.google.com.mx/";
+        public static string baseURL = "https://fs19.formsite.com/vTUJhX/upkauxsphd/index.html";
 
         [TestMethod]
         public void FirefoxTest()
@@ -21,15 +21,50 @@ namespace QA_hw2
 
                 WebDriver.Navigate().GoToUrl(baseURL);
 
-                var searchBox = WebDriver.FindElementByName("q");
+                var firstName = WebDriver.FindElementById("RESULT_TextField-2");
 
-                searchBox.SendKeys("automation using selenium 3.0 in C#");
+                firstName.SendKeys("Marco");
 
-                searchBox.Submit();
+                var lastName = WebDriver.FindElementById("RESULT_TextField-3");
 
-                WebDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+                lastName.SendKeys("Serrano");
+
+                var address = WebDriver.FindElementById("RESULT_TextField-4");
+
+                address.SendKeys("123 Street St.");
+
+                var address2 = WebDriver.FindElementById("RESULT_TextField-5");
+
+                address2.SendKeys("Apt 123");
+
+                var city = WebDriver.FindElementById("RESULT_TextField-6");
+
+                city.SendKeys("Townsville");
+
+                // State/Province
+                var state = WebDriver.FindElementById("RESULT_RadioButton-7");
+
+                // do
+
+                var zipCode = WebDriver.FindElementById("RESULT_TextField-8");
+
+                zipCode.SendKeys("77777");
+
+                var phone = WebDriver.FindElementById("RESULT_TextField-9");
+
+                phone.SendKeys("5557771234");
+
+                var email = WebDriver.FindElementById("RESULT_TextField-10");
+
+                email.SendKeys("marco@marco.com");
+
+                var search = WebDriver.FindElementById("FSsubmit");
+
+                search.Submit();
+
+                //WebDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
                 
-                var searchResults = WebDriver.FindElementById("resultStats");
+               // var searchResults = WebDriver.FindElementById("resultStats");
 
                 WebDriver.Close();
             }
@@ -44,13 +79,50 @@ namespace QA_hw2
 
                 WebDriver.Navigate().GoToUrl(baseURL);
 
-                var searchBox = WebDriver.FindElementByName("q");
+                var firstName = WebDriver.FindElementById("RESULT_TextField-2");
 
-                searchBox.SendKeys("Automation using selenium 3.0 in C#");
+                firstName.SendKeys("Marco");
 
-                searchBox.Submit();
+                var lastName = WebDriver.FindElementById("RESULT_TextField-3");
 
-                var searchResults = WebDriver.FindElementById("resultStats");
+                lastName.SendKeys("Serrano");
+
+                var address = WebDriver.FindElementById("RESULT_TextField-4");
+
+                address.SendKeys("123 Street St.");
+
+                var address2 = WebDriver.FindElementById("RESULT_TextField-5");
+
+                address2.SendKeys("Apt 123");
+
+                var city = WebDriver.FindElementById("RESULT_TextField-6");
+
+                city.SendKeys("Townsville");
+
+                // State/Province
+                var state = WebDriver.FindElementById("RESULT_RadioButton-7");
+
+                // do
+
+                var zipCode = WebDriver.FindElementById("RESULT_TextField-8");
+
+                zipCode.SendKeys("77777");
+
+                var phone = WebDriver.FindElementById("RESULT_TextField-9");
+
+                phone.SendKeys("5557771234");
+
+                var email = WebDriver.FindElementById("RESULT_TextField-10");
+
+                email.SendKeys("marco@marco.com");
+
+                var search = WebDriver.FindElementById("FSsubmit");
+
+                search.Submit();
+
+                //WebDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+                // var searchResults = WebDriver.FindElementById("resultStats");
 
                 WebDriver.Close();
             }
